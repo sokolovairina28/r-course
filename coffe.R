1ff311f71b852bc74d78e9098418fa442fd664a4
Irina <ivasheevairina@yandex.ru>
2026-09-24 22:02:40 +0300

    Добавил файлы курса по R

---
+++ coffe.R
@@ -0,0 +1,40 @@
+1.5 * 100 / 2
+1.5 * 100 / 2 / 2
+1.5 * 100 / 2 / 2 / 2
+
+---
+title: "Пингвины с острова Dream"
+format:
+  html:
+    embed-resources: true
+---
+
+install.packages(c("tidyverse"
+ broom
+ epiR
+ Hmisc
+ lmtest
+ multcomp
+ pROC
+ rstatix
+ sandwich
+ TrialSize
+ yardstick
+ FactoMineR
+ factoextra
+ ggbiplot
+ dagitty
+ggdag
+ corrplot
+ GGally
+ ggExtra
+ ggfortify
+ ggpubr
+ pheatmap
+ plotly
+ flextable
+ openxlsx
+ gtsummary
+ devtools
+ knitr
+))
